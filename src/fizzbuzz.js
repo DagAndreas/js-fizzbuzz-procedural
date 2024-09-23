b7b7dbc09797ed3a3bb4ac48578619b9f010febb
Dag Andreas <daanfofo@gmail.com>
2024-09-23 14:33:48 +0200

    completed fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,23 @@ const answer = []
 
 // Write your code below this line
 
+const fizz = 3
+const buzz = 5
 
+for (let i = 1; i <= 15; i++){
+    if (i % fizz === 0 && i % buzz === 0){
+        answer.push('FizzBuzz')
+    }
+    else if (i % fizz == 0){
+        answer.push('Fizz')
+    }
+    else if (i % buzz == 0){
+        answer.push('Buzz')
+    }
+    else {
+        answer.push(i)
+    }
+}
 
 
 
